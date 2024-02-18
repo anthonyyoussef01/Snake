76d35f193e26c3300785db8189abd964cff30571
antho <anthonyyoussef01@gmail.com>
2024-02-18 17:03:34 -0500

    Added doc comment for constructor

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
+    // This is the constructor for the GameFrame class
+    // It adds a new GamePanel to the frame, sets the title of the frame to "Snake",
+    // sets the default close operation to exit on close, sets the frame to not be resizable,
+    // packs the frame, sets the frame to be visible, and sets the location of the frame to the center of the screen
     public GameFrame() {
         this.add(new GamePanel());
         this.setTitle("Snake");
