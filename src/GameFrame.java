357e459393a6758205e5d87797509185f32f3c6f
antho <anthonyyoussef01@gmail.com>
2024-02-18 16:59:45 -0500

    empty constructors

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,4 +1,10 @@
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
+    public GameFrame() {
+    }
+
+    public static void main(String[] args) {
+        new GameFrame();
+    }
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,8 +3,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GamePanel extends JPanel implements ActionListener {
+    GamePanel() {
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
-
     }
 }
