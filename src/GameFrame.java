64cad54e10c1dc602c5f1407f83da0ba95a5861d
antho <anthonyyoussef01@gmail.com>
2024-02-18 17:02:01 -0500

    Set up GameFrame

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -2,6 +2,14 @@ import javax.swing.*;
 
 public class GameFrame extends JFrame {
     public GameFrame() {
+        GamePanel panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
     }
 
     public static void main(String[] args) {
